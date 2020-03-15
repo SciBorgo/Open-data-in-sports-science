b02142c33fa6ee81984f1ed0b2dfca23d2bc08d8
Joshua Bon <joshuajbon@gmail.com>
2020-03-16 09:35:24 +1000

    Beginning to automate workflow

--- random-sample-analysis.R
+++ random-sample-analysis.R
@@ -1,15 +1,24 @@
-
-
 #### Open data and code in sports science analysis
+# Author: David Borg
+# Contributor: Joshua J Bon
+# Data collection and processing: David Borg, Brenton J Baguley
+#
+
+## Simple random sample to choose articles to survey.
 
 d = read.csv("total-articles-after-title-screen.csv")
 
+## Choose random sample
 # Select 400
 Number = sort(sample(x = d$Number, size = 400, replace = F, set.seed(123)))
 sample = data.frame(Number)
 
 selected <- merge(sample, d, by="Number")
+
+# procedure to sample, complete once.
 #write.csv(selected, file = "random-sample-400-data-extraction.csv")
+
+## Calculate proportions and CIS from articles:
 
 #install.packages(c("binom","dplyr"))
 library(binom)
@@ -22,30 +31,34 @@ d <- d %>% filter(Eligble == "1")
 table(d$Topic_area) # How many articles
 
 # Shared data
-table(d$Data_shared)
-successes <- 13
-trials <- 299
+table(d$Data_shared, useNA = 'ifany')
+successes <- sum(d$Data_shared == 1, na.rm = T)
+trials <- sum(!is.na(d$Data_shared))
 binom.confint(x = successes, n = trials, conf.level = 0.95, methods = "exact")
 
 # Shared code
-table(d$Code_shared) # Zero shared code
+table(d$Code_shared, useNA = 'ifany')
+# !!comment to explain NAs
+# Zero shared code
 
 # Data available upon request
-table(d$Data_available_upon_request)
-successes <- 5
-trials <- 286
+table(d$Data_available_upon_request, useNA = 'ifany')
+# !!comment to explain NAs
+successes <- sum(d$Data_available_upon_request == 1, na.rm = T)
+trials <- sum(!is.na(d$Data_available_upon_request))
 binom.confint(x = successes, n = trials, conf.level = 0.95, methods = "exact")
 
 # Statistical software used
-table(d$Software)
-trials <- 298
-R <- binom.confint(x = 25, n = trials, conf.level = 0.95, methods = "exact"); R
-SAS <- binom.confint(x = 18, n = trials, conf.level = 0.95, methods = "exact"); SAS
-SPSS <- binom.confint(x = 127, n = trials, conf.level = 0.95, methods = "exact"); SPSS
-Stata <- binom.confint(x = 19, n = trials, conf.level = 0.95, methods = "exact"); Stata
-Other <- binom.confint(x = 46, n = trials, conf.level = 0.95, methods = "exact"); Other
-NS <- binom.confint(x = 64, n = trials, conf.level = 0.95, methods = "exact"); NS
+table(d$Software, useNA = 'ifany')
+trials <- sum(!is.na(d$Software)) # this was 298, but now 299?
+R <- binom.confint(x = sum(d$Software == "R"), n = trials, conf.level = 0.95, methods = "exact"); R
+SAS <- binom.confint(x = sum(d$Software == "SAS"), n = trials, conf.level = 0.95, methods = "exact"); SAS
+SPSS <- binom.confint(x = sum(d$Software == "SPSS"), n = trials, conf.level = 0.95, methods = "exact"); SPSS
+Stata <- binom.confint(x = sum(d$Software == "Stata"), n = trials, conf.level = 0.95, methods = "exact"); Stata
+Other <- binom.confint(x = sum(d$Software == "Other"), n = trials, conf.level = 0.95, methods = "exact"); Other
+NS <- binom.confint(x = sum(d$Software == "Not_stated"), n = trials, conf.level = 0.95, methods = "exact"); NS
 
+# Continue editing here:
 
 #### Proportions for 'sport and exercise science' topic area
 ###
